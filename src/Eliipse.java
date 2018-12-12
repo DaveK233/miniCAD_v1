4dce79d52ec6e92c15b3c357d6c61ab4a38bec65
Dawei <mrscarf1@outlook.com>
2018-12-12 20:17:22 +0800

    updated Items

--- src/Eliipse.java
+++ src/Eliipse.java
@@ -1,2 +1,29 @@
-public class Eliipse {
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class Eliipse extends Items {
+
+    public Eliipse(Graphics2D g2d, Color icolor, float width, double x1, double y1, double x2, double y2) {
+        super(g2d, icolor, width, x1, y1, x2, y2);
+    }
+
+    @Override
+    public void drawItems() {
+
+    }
+
+    @Override
+    public void Resize(Point2D point1, Point2D point2, int centre) {
+
+    }
+
+    @Override
+    public void Move(Point2D point) {
+
+    }
+
+    @Override
+    public String ReadItem() {
+        return null;
+    }
 }
