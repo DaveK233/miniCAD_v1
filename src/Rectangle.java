4464524a97ed0f71ebe987b014ec222e248b6348
Dawei <mrscarf1@outlook.com>
2018-12-12 16:21:30 +0800

    updated rectangle

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,9 +1,13 @@
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class Rectangle extends Items {
+    private Rectangle2D rectangle;
     public Rectangle(Graphics2D g2d, Color icolor, float width, double x1, double y1, double x2, double y2) {
         super(g2d, icolor, width, x1, y1, x2, y2);
+        rectangle = new Rectangle2D.Double(x1, y1, x2, y2);
+
     }
 
     @Override
